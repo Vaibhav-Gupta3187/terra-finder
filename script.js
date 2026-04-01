5654481406bb395ad816e9e715a942c8a2b52001
Vaibhav-Gupta318 <vaibhav.g35490@nst.rishihood.edu.in>
2026-04-01 23:52:12 +0530

    Added search functionality

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-const container = document.getElementById("data");
+let countriesData = [];
 
-fetch("https://restcountries.com/v3.1/all?fields=name,flags,capital,population")
+// Fetch API data
+fetch("https://restcountries.com/v3.1/all")
   .then(res => res.json())
   .then(data => {
-
-    // limit to 20 countries (clean UI)
-    data.slice(0, 20).forEach(country => {
-      const div = document.createElement("div");
-
-      div.innerHTML = `
-        <h3>${country.name.common}</h3>
-        <img src="${country.flags.png}" width="100"/>
-        <p>Capital: ${country.capital ? country.capital[0] : "N/A"}</p>
-        <p>Population: ${country.population}</p>
-        <hr/>
-      `;
-
-      container.appendChild(div);
-    });
-
-  })
-  .catch(err => {
-    console.log(err);
-    container.innerText = "Failed to load data";
+    countriesData = data;
+    displayCountries(data);
   });
+
+// Display countries
+function displayCountries(data) {
+  const container = document.getElementById("countriesContainer");
+  container.innerHTML = "";
+
+  data.map(country => {
+    const div = document.createElement("div");
+
+    div.style.border = "1px solid black";
+    div.style.margin = "10px";
+    div.style.padding = "10px";
+    div.style.display = "inline-block";
+
+    div.innerHTML = `
+      <h3>${country.name.common}</h3>
+      <img src="${country.flags.png}" width="100"/>
+      <p><b>Capital:</b> ${country.capital ? country.capital[0] : "N/A"}</p>
+      <p><b>Population:</b> ${country.population}</p>
+      <p><b>Region:</b> ${country.region}</p>
+    `;
+
+    container.appendChild(div);
+  });
+}
+
+// Search functionality
+document.getElementById("searchInput").addEventListener("input", function () {
+  const searchValue = this.value.toLowerCase();
+
+  const filteredCountries = countriesData.filter(country =>
+    country.name.common.toLowerCase().includes(searchValue)
+  );
+
+  displayCountries(filteredCountries);
+});
